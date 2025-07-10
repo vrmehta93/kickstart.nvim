57e8892c87fc73dfaa9287942651bca2c0971ab6
Vishal Mehta <6668870+vrmehta93@users.noreply.github.com>
2025-07-09 18:43:58 -0700

    Update comments

--- init.lua
+++ init.lua
@@ -876,6 +876,7 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
+        -- NOTE - these formatters need to be installed via LSP/Mason
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
