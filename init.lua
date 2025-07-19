c12851634a46811c19e159e306af963e364a514c
Vishal Mehta <6668870+vrmehta93@users.noreply.github.com>
2025-07-18 22:27:06 -0700

    Add tmux file error checking

--- init.lua
+++ init.lua
@@ -258,6 +258,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- My Autocommands
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'tmux',
+  callback = function()
+    vim.keymap.set('n', '<leader>ct', ':!tmux source %<CR>', { buffer = true, desc = 'Error check tmux file' })
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
